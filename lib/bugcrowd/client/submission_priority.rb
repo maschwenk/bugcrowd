dd011a802c54a327fc0b697800aaedbddfbb37f5
Max Schwenk <maschwenk@gmail.com>
2017-02-21 16:33:36 -0800

    Endpoint does not accept query params

--- lib/bugcrowd/client/submission_priority.rb
+++ lib/bugcrowd/client/submission_priority.rb
@@ -1,20 +1,20 @@
 module Bugcrowd
   class Client
     module SubmissionPriorities
-      def get_submission_priority(submission_uuid, options={})
-        response = Client.new.get("/submissions/#{submission_uuid}/priority", query: options)
+      def get_submission_priority(submission_uuid)
+        response = Client.new.get("/submissions/#{submission_uuid}/priority")
       end
 
-      def set_submission_priority(submission_uuid, priority, options={})
-        response = Client.new.post("/submissions/#{submission_uuid}/priority", priority_params(priority), query: options)
+      def set_submission_priority(submission_uuid, priority)
+        response = Client.new.post("/submissions/#{submission_uuid}/priority", priority_params(priority))
       end
 
-      def update_submission_priority(submission_uuid, priority, options={})
-       response = Client.new.put("/submissions/#{submission_uuid}/priority", priority_params(priority), query: options)
+      def update_submission_priority(submission_uuid, priority)
+       response = Client.new.put("/submissions/#{submission_uuid}/priority", priority_params(priority))
       end
 
-      def remove_submission_priority(submission_uuid, options={})
-        response = Client.new.delete("/submissions/#{submission_uuid}/priority", query: options)
+      def remove_submission_priority(submission_uuid)
+        response = Client.new.delete("/submissions/#{submission_uuid}/priority")
       end
 
       private
